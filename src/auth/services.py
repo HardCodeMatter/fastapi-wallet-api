c58290f53db514853268fa3159f06544dbbdbed8
HardCodeMatter <dklymovets1@gmail.com>
2024-04-12 20:27:04 +0300

    Added functionality to user service.

--- src/auth/services.py
+++ src/auth/services.py
@@ -1,11 +1,54 @@
+from fastapi import HTTPException, status
+from sqlalchemy import select
+
 from services import BaseService
+from .models import User
 
 
 class UserService(BaseService):
-    def create_user(self): ...
+    async def create_user(self): ...
 
-    def get_user_by_uuid(self): ...
+    async def get_user_by_uuid(self, uuid: str) -> User:
+        user = await self.session.execute(
+            select(User).filter(User.uuid == uuid)
+        )
 
-    def get_user_by_username(self): ...
+        if not user:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail='User with this UUID is not found.',
+            )
+        
+        await self.session.commit()
 
-    def get_user_by_email(self): ...
+        return user.scalar()
+
+    async def get_user_by_username(self, username: str) -> User:
+        user = await self.session.execute(
+            select(User).filter(User.username == username)
+        )
+
+        if not user:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail='User with this username is not found.',
+            )
+        
+        await self.session.commit()
+
+        return user.scalar()
+
+    async def get_user_by_email(self, email: str):
+        user = await self.session.execute(
+            select(User).filter(User.email == email)
+        )
+
+        if not user:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail='User with this email is not found.',
+            )
+        
+        await self.session.commit()
+
+        return user.scalar()
