f7adf5f274da28393fbf92de89725906a061c53a
HardCodeMatter <dklymovets1@gmail.com>
2024-04-13 17:43:37 +0300

    Added token schemas, created get_current_user and 'active' functions.

--- src/auth/utils.py
+++ src/auth/utils.py
@@ -1,5 +1,15 @@
+from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.ext.asyncio import AsyncSession
 from passlib.context import CryptContext
+from jose import jwt, JWTError
+
+from database.database import get_async_session
+
+from .config import auth_settings
+from .models import User
+from .schemas import TokenData
+from .services import UserService
 
 
 password_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
@@ -12,4 +22,45 @@ def hash_password(password: str) -> str:
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return password_context.verify(plain_password, hashed_password)
 
-def get_current_user(): ...
+async def get_current_user(
+    token: str = Depends(oauth2_scheme),
+    session: AsyncSession = Depends(get_async_session)
+) -> User:
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401,
+        detail='Invalid credentials.',
+        headers={'WWW-Authenticate': 'Bearer'},
+    )
+
+    try:
+        payload = jwt.decode(
+            token=token,
+            key=auth_settings.SECRET_KEY,
+            algorithms=auth_settings.ALGORITHM,
+        )
+
+        username = payload.get('sub')
+
+        if not username:
+            raise credentials_exception
+        
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    
+    user = await UserService(session).get_user_by_username(token_data.username)
+
+    if not user:
+        raise credentials_exception
+    
+    return user
+
+
+async def get_current_active_user(current_user: User = Depends(get_current_user)):
+    if not current_user.is_active:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail='User is inactive.',
+        )
+    
+    return current_user
